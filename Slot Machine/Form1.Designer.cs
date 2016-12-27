072c1b0369d72e370fb54a7a05d945b496628928
Mario Dudjak <dudjakmario@hotmail.com>
2016-12-27 12:17:39 +0100

    Second commit

--- Slot Machine/Form1.Designer.cs
+++ Slot Machine/Form1.Designer.cs
@@ -45,6 +45,7 @@ namespace Slot_Machine
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
@@ -196,12 +197,23 @@ namespace Slot_Machine
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(593, 296);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 62);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "WITHDRAWN MONEY";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(952, 564);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -244,6 +256,7 @@ namespace Slot_Machine
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
     }
 }
 
